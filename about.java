d0049d90680a55d127be766d6a007203c16e6498
thejeshkum <psivakumar724@gmail.com>
2024-03-05 10:28:41 +0000

    about my self

---
+++ about.java
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=5.0">
+    <title>shiva</title>
+    
+
+</head>
+<body>
+<h1>HI THIS IS SHIVA FROM SARANGAL</h1>
+<h1>AM FROM SARANGAL </h1>
+<h1>I COMPLETED DEGREE IN B.COM </h1>
+<h1>I LOVED TO WORK AS AN DEVOPS ENGENIER</h1> 
+<img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+</body>
+</html>
+
